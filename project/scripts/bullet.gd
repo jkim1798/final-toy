37ad83e708b421cce60c44be780dabdbc8343dba
Juhyun Kim <46714@jisedu.or.id>
2024-04-24 19:51:39 +0700

    final changes

--- project/scripts/bullet.gd
+++ project/scripts/bullet.gd
@@ -11,6 +11,7 @@ func _physics_process(delta):
 func parry(rotation: float):
 	velocity = Vector2.LEFT.rotated(rotation) * bullet_speed * -1.5
 	$Area2D.collision_mask = 3
+	$Sprite2D.self_modulate = Color("#0099db")
 
 func _on_area_2d_body_entered(body):
 	if body == self:
